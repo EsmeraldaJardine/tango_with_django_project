029f69cc71de3b588db241a003016122203a8a6a
Esmeralda Jardine <2391491j@student.gla.ac.uk>
2024-03-19 11:52:34 +0000

    end of chapter 4

--- rango/views.py
+++ rango/views.py
@@ -7,13 +7,15 @@ from django.http import HttpResponse
 # a user to see your view, you must map a Uniform Resource Locator (URL)⁹ to the view
 
 def index(request):
-    about_link = "<a href='/rango/about/'>About</a>"
-    return HttpResponse(f"Rango says hey there partner!{about_link}")
-# The f at the beginning of the string indicates that it is an f-string, 
-# and the expressions inside the curly braces will be evaluated and 
-# formatted at runtime.
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage matches to {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render(request, 'rango/index.html', context=context_dict)
 
 
 def about(request):
-    index_link = "<a href='/rango/'>Index</a>"
-    return HttpResponse(f"Rango says here is the about page. {index_link}")
+    context_dict = {'name': 'Esmeralda'}
+    return render(request, 'rango/about.html', context=context_dict)
