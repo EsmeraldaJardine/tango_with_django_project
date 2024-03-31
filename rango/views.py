b5421437617f88b48e298f8ace6a675dd73c974b
Esmeralda Jardine <2391491j@student.gla.ac.uk>
2024-03-31 07:53:15 +0100

    chapter 10: all passed - fixied visitor_cookie_handler()

--- rango/views.py
+++ rango/views.py
@@ -23,9 +23,10 @@ def index(request):
 
 
 def about(request):
+    context_dict = {}
     visitor_cookie_handler(request)
-    visits = request.session['visits']
-    context_dict = {'name': 'Esmeralda', 'visits': visits}
+    context_dict['name'] = 'Esmeralda'
+    context_dict['visits'] = request.session['visits']
     
     return render(request, 'rango/about.html', context=context_dict)
 
@@ -140,15 +141,15 @@ def user_logout(request):
     return redirect(reverse('rango:index'))
 
 def visitor_cookie_handler(request):
-    visits = int(request.COOKIES.get('visits', '1'))
-    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
-    last_visit_time = datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
-
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+    
     if (datetime.now() - last_visit_time).days > 0:
         visits = visits + 1
-        request.session['visits'] = str(datetime.now())
+        request.session['last_visit'] = str(datetime.now())
     else:
-        request.session['visits'] = last_visit_cookie
+        request.session['last_visit'] = last_visit_cookie
     
     request.session['visits'] = visits
 
