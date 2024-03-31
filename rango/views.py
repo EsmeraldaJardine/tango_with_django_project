0112b493b10a9195e25201a68d238b427be11494
Esmeralda Jardine <2391491j@student.gla.ac.uk>
2024-03-31 05:31:24 +0100

    chapter 9 exercise: 2 test failures

--- rango/views.py
+++ rango/views.py
@@ -40,6 +40,7 @@ def show_category(request, category_name_slug):
 
     return render(request, 'rango/category.html', context=context_dict)
 
+@login_required
 def add_category(request):
     form = CategoryForm()
     if request.method == 'POST':
@@ -54,6 +55,7 @@ def add_category(request):
     return render(request, 'rango/add_category.html', {'form': form})
 
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -105,9 +107,7 @@ def register(request):
         user_form = UserForm()
         profile_form = UserProfileForm()
     
-    return render(request, 'rango/register.html', context={'user_form': user_form, 
-                                                           'profile_form': profile_form, 
-                                                           'registered': registered})
+    return render(request, 'rango/register.html', context={'user_form': user_form,'profile_form': profile_form, 'registered': registered})
 
 def user_login(request):
     if request.method == 'POST':
@@ -130,7 +130,7 @@ def user_login(request):
     
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html')
 
 @login_required
 def user_logout(request):
